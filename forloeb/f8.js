10868c47a715c9a14cfe9e806249cf50423f0de9
Brian Veis Pennerup <brian.veis.pennerup@protonmail.com>
2026-04-24 22:51:48 +0200

    F8: Start x-udsnit -6 til 6, data til ±100 så man kan zoome ud

--- forloeb/f8.js
+++ forloeb/f8.js
@@ -98,8 +98,8 @@ GENERATORS.F8 = [
     const yMax  = Math.max(...yVals);
     const layout = Object.assign({}, PLOTLY_LAYOUT_BASE, {
       height: 320, showlegend: true, legend: { x: 0.02, y: 0.98 }, dragmode: 'pan',
-      xaxis: Object.assign({}, PLOTLY_LAYOUT_BASE.xaxis),
-      yaxis: Object.assign({}, PLOTLY_LAYOUT_BASE.yaxis)
+      xaxis: Object.assign({}, PLOTLY_LAYOUT_BASE.xaxis, { range: [-6, 6], autorange: false }),
+      yaxis: Object.assign({}, PLOTLY_LAYOUT_BASE.yaxis, { autorange: true })
     });
     return {
       type: "mc2",
